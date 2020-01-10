815a480e8241acb6fe24f21e110b74e1562550b7
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-01-10 11:15:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
 // Code your solution here!
+function printString(string) {
+  if (string.length < 2) {
+    console.log(string)
+    return
+  } else {
+    console.log(string[0])
+    printString(string.substring(1))
+  }
+}
 
+function reverseString(string) {
+  if (string.length < 2) {
+    return string
+  } else {
+    return string.slice(-1) + reverseString(string.substring(0, string.length - 1))
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length < 2) {
+    return true
+  } else {
+    if (string[0] === string[string.length - 1]) {
+      return isPalindrome(string.slice(1, string.length - 1))
+    } else {
+      return false
+    }
+  }
+}
+
+function addUpTo(nums, index) {
+  if (index === 0) {
+    return nums[0]
+  } else {
+    return nums[index] + addUpTo(nums, index - 1)
+  }
+}
+
+function maxOf(nums) {
+  if (nums.length < 2) {
+    return nums[0]
+  } else {
+    return Math.max(nums[0], maxOf(nums.slice(1)))
+  }
+}
+
+function includesNumber(nums, num) {
+  if (nums.length < 2) {
+    if (nums[0] === num) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    if (nums[0] === num) {
+      return true
+    } else {
+      return includesNumber(nums.slice(1), num)
+    }
+  }
+}
